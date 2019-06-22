480967e83507c3fef40bc68b77fff354f2677109
Sophie Machin <sophiemachin@gmail.com>
2019-06-22 14:52:01 +0100

    EnTableToolbar: remove highlight for selected

--- frontend/src/table/EnTableToolbar.js
+++ frontend/src/table/EnTableToolbar.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import {makeStyles} from "@material-ui/core";
-import {lighten} from "@material-ui/core/styles";
 import Toolbar from "@material-ui/core/Toolbar";
-import clsx from "clsx";
 import Typography from "@material-ui/core/Typography";
 import Tooltip from "@material-ui/core/Tooltip";
 import IconButton from "@material-ui/core/IconButton";
@@ -13,16 +11,6 @@ const useToolbarStyles = makeStyles(theme => ({
     paddingLeft: theme.spacing(2),
     paddingRight: theme.spacing(1),
   },
-  highlight:
-    theme.palette.type === 'light'
-      ? {
-        color: theme.palette.secondary.main,
-        backgroundColor: lighten(theme.palette.secondary.light, 0.85),
-      }
-      : {
-        color: theme.palette.text.primary,
-        backgroundColor: theme.palette.secondary.dark,
-      },
   spacer: {
     flex: '1 1 100%',
   },
@@ -36,13 +24,11 @@ const useToolbarStyles = makeStyles(theme => ({
 
 const EnhancedTableToolbar = props => {
   const classes = useToolbarStyles();
-  const { numSelected, title } = props;
+  const { title } = props;
 
   return (
     <Toolbar
-      className={clsx(classes.root, {
-        [classes.highlight]: numSelected > 0,
-      })}
+      className={classes.root}
     >
       <div className={classes.title}>
         <Typography variant="h6" id="tableTitle">
