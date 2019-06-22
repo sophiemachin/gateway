b1fd3dba845a0c9ffd0ca1324afc23747c1b1e72
Sophie Machin <sophiemachin@gmail.com>
2019-06-22 12:59:58 +0100

    App.js: generic landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
-import UsersList from './UsersList.js'
 
 function App() {
   return (
     <div className="App">
-      <UsersList/>
+      This is the home page
     </div>
   );
 }
