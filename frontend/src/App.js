4af7dbcbe75c2a3893ddad43c68516a660a17987
Sophie Machin <sophiemachin@gmail.com>
2019-06-23 20:19:27 +0100

    start login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import './App.css';
+import Button from "@material-ui/core/Button";
+import Input from "@material-ui/core/Input";
+import Card from "@material-ui/core/Card";
 
 function App() {
+
+  const [username, setUserName] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  function onChangeUsername(username) {
+    setUserName(username)
+  }
+
+  function onChangePassword(password) {
+    setPassword(password)
+  }
+
   return (
     <div className="App">
+
       This is the home page
+
+      <Card>
+        <Button href='/#/users'>Go to admin page</Button>
+      </Card>
+
+      <Card>
+
+        <Input
+          placeholder="Username"
+          onChange={e => onChangeUsername(e.target.value)}
+        />
+        <Input
+          placeholder="Password"
+          onChange={e => onChangePassword(e.target.value)}
+          type='password'
+        />
+        <Button href='/#/users'>Go to admin page</Button>
+      </Card>
     </div>
   );
 }
