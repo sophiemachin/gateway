75abeb678dc66b4d40a21805f7bdff10f6f086cb
Sophie Machin <sophiemachin@gmail.com>
2019-06-23 19:55:03 +0100

    add breadcrumbs

--- frontend/src/VariantsList.js
+++ frontend/src/VariantsList.js
@@ -1,23 +1,61 @@
 import React from 'react';
 import {makeStyles, Typography as T} from "@material-ui/core";
 import Card from "@material-ui/core/Card";
+import Link from "@material-ui/core/Link";
+import {getDrName, getPatientName} from "./formattingHelpers";
+
+import users from "./data/users";
+import patients from "./data/patients";
+
+
+function getUser(userId){
+  return users.filter(user => user.id.toString() === userId.toString())[0];
+}
+
+function getPatient(patientId){
+  return patients.filter(p => p.id.toString() === patientId.toString())[0];
+}
 
 
 const useCardStyles = makeStyles(theme => ({
+  root: {
+    margin: theme.spacing(3),
+    // display: 'flex',
+  },
   card: {
     padding: theme.spacing(2),
-    margin: 'auto',
-    marginTop: theme.spacing(5),
+    // margin: 'auto',
+    // marginTop: theme.spacing(5),
   },
+  content: {
+    display: 'flex',
+  }
 }));
 
 
 
-export default function EnhancedTable() {
+export default function EnhancedTable(props) {
+  const { ids } = props
   const classes = useCardStyles()
   return (
-    <div style={{display:'flex'}}>
-      <Card className={classes.card}><T>Not implemented</T></Card>
+    <div className={classes.root}>
+
+
+      <Card className={classes.card}>
+        <T variant="h6">
+        <div>
+          <Link href={`/#/users/${ids.userId}/patients`}>
+            {getDrName(getUser(ids.userId))}
+          </Link> › <Link href={`/#/users/${ids.userId}/patients/${ids.patientId}/samples`}>
+          {getPatientName(getPatient(ids.patientId))}
+        </Link> › variants
+        </div>
+        </T>
+
+        <div className={classes.content}>
+          <T>Not implemented</T>
+        </div>
+      </Card>
     </div>
   );
 }
