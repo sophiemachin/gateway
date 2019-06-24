624b4c764d7a99b526548c8cd275890b1039d4b0
sem <sem@pearson.co.uk>
2019-06-24 12:41:43 +0100

    Updates

---
+++ precache-manifest.50c4f37ccf5c8bb85fbf44b4f8e0903f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "811483f3c0a8a15b4806b848b71da0a5",
+    "url": "/gateway/index.html"
+  },
+  {
+    "revision": "4a26295bda1ed8b0e9bb",
+    "url": "/gateway/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "e29e2b53c2055042ec1b",
+    "url": "/gateway/static/js/2.f109e57b.chunk.js"
+  },
+  {
+    "revision": "4a26295bda1ed8b0e9bb",
+    "url": "/gateway/static/js/main.51838aa2.chunk.js"
+  },
+  {
+    "revision": "49ff926f3908ef826388",
+    "url": "/gateway/static/js/runtime~main.d9f4adce.js"
+  },
+  {
+    "revision": "fc3581c523c23f245e12571e3f57f55e",
+    "url": "/gateway/static/media/logo.fc3581c5.png"
+  }
+]);
